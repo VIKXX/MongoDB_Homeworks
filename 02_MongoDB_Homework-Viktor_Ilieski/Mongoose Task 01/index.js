9e362ec9040abe1aede5e3d8c1976e71e662f34a
Your Name <you@example.com>
2024-07-24 17:45:52 +0200

    Second Homework

--- 02_MongoDB_Homework-Viktor_Ilieski/Mongoose Task 01/index.js
+++ 02_MongoDB_Homework-Viktor_Ilieski/Mongoose Task 01/index.js
@@ -4,43 +4,43 @@ const BookStore = require("./bookstore");
 mongoose.connect("mongodb+srv://viktor_use01:viktor369551@cluster0.ogeqvpp.mongodb.net/BookStoreDatabase?retryWrites=true&w=majority&appName=Cluster0")
 
 
-// async function insertBookStore() {
-//     try {
-//         const inserBook = new BookStore({
-//             title: "Odyssey",
-//             author: "Homer",
-//             country: "Greece",
-//             language: "Greek",
-//             pages: 374,
-//             year: -800,
-//             imageLink: "images/the-odyssey-of-homer.jpg",
-//             wikipediaLink: "https://en.wikipedia.org/wiki/Odyssey\n"
-//         });
-//         await inserBook.save()
-//         console.log(`Book "${inserBook.title}" is already inserted!`)
-//     } catch (error) {
-//         console.log(error.message);
-//         throw error
-//     }
+async function insertBookStore() {
+    try {
+        const inserBook = new BookStore({
+            title: "Odyssey",
+            author: "Homer",
+            country: "Greece",
+            language: "Greek",
+            pages: 374,
+            year: -800,
+            imageLink: "images/the-odyssey-of-homer.jpg",
+            wikipediaLink: "https://en.wikipedia.org/wiki/Odyssey\n"
+        });
+        await inserBook.save()
+        console.log(`Book "${inserBook.title}" is already inserted!`)
+    } catch (error) {
+        console.log(error.message);
+        throw error
+    }
 
-// }
+}
 
-// insertBookStore()
+insertBookStore()
 
 
 
-// async function getBookStore() {
-//     try {
-//         const getBooks = await BookStore.findOne({ title: "Odyssey" })
-//         console.log(getBooks);
+async function getBookStore() {
+    try {
+        const getBooks = await BookStore.findOne({ title: "Odyssey" })
+        console.log(getBooks);
 
-//     } catch (error) {
-//         console.log(error.message);
-//         throw error
-//     }
-// }
+    } catch (error) {
+        console.log(error.message);
+        throw error
+    }
+}
 
-// getBookStore()
+getBookStore()
 
 
 async function updateBookStore() {
--- 02_MongoDB_Homework-Viktor_Ilieski/Mongoose Task 02/index.js
+++ 02_MongoDB_Homework-Viktor_Ilieski/Mongoose Task 02/index.js
@@ -4,79 +4,79 @@ const recipesStore = require("./recipes")
 mongoose.connect("mongodb+srv://viktor_use01:viktor369551@cluster0.ogeqvpp.mongodb.net/RecipeStoreDatabase?retryWrites=true&w=majority&appName=Cluster0")
 
 
-// async function insertRecipeStore() {
-//     try {
-//         const insertRecipe = new recipesStore({
-//             name: "Crock Pot Roast",
-//             ingredients: [
-//                 {
-//                     quantity: "1",
-//                     name: " beef roast",
-//                     type: "Meat"
-//                 },
-//                 {
-//                     quantity: "1 package",
-//                     name: "brown gravy mix",
-//                     type: "Baking"
-//                 },
-//                 {
-//                     quantity: "1 package",
-//                     name: "dried Italian salad dressing mix",
-//                     type: "Condiments"
-//                 },
-//                 {
-//                     quantity: "1 package",
-//                     name: "dry ranch dressing mix",
-//                     type: "Condiments"
-//                 },
-//                 {
-//                     quantity: "1/2 cup",
-//                     name: "water",
-//                     type: "Drinks"
-//                 }
-//             ],
-//             steps: [
-//                 {
-//                     stepsDescription: "Place beef roast in crock pot.",
-//                 },
-//                 {
-//                     stepsDescription: "Mix the dried mixes together in a bowl and sprinkle over the roast.",
-//                 },
-//                 {
-//                     stepsDescription: "Pour the water around the roast.",
-//                 },
-//                 {
-//                     stepsDescription: "Cook on low for 7-9 hours.",
-//                 },
-//             ],
-//             cookingTime: "420min",
-//             imageURL: "http://img.sndimg.com/food/image/upload/w_266/v1/img/recipes/27/20/8/picVfzLZo.jpg",
-//             originalURL: "http://www.food.com/recipe/to-die-for-crock-pot-roast-27208"
-//         });
-//         await insertRecipe.save()
-//         console.log(`Recipe "${insertRecipe.name}" is already inserted!`);
-//     } catch (error) {
-//         console.log(error.message);
-//         throw error
-//     }
-// }
+async function insertRecipeStore() {
+    try {
+        const insertRecipe = new recipesStore({
+            name: "Crock Pot Roast",
+            ingredients: [
+                {
+                    quantity: "1",
+                    name: " beef roast",
+                    type: "Meat"
+                },
+                {
+                    quantity: "1 package",
+                    name: "brown gravy mix",
+                    type: "Baking"
+                },
+                {
+                    quantity: "1 package",
+                    name: "dried Italian salad dressing mix",
+                    type: "Condiments"
+                },
+                {
+                    quantity: "1 package",
+                    name: "dry ranch dressing mix",
+                    type: "Condiments"
+                },
+                {
+                    quantity: "1/2 cup",
+                    name: "water",
+                    type: "Drinks"
+                }
+            ],
+            steps: [
+                {
+                    stepsDescription: "Place beef roast in crock pot.",
+                },
+                {
+                    stepsDescription: "Mix the dried mixes together in a bowl and sprinkle over the roast.",
+                },
+                {
+                    stepsDescription: "Pour the water around the roast.",
+                },
+                {
+                    stepsDescription: "Cook on low for 7-9 hours.",
+                },
+            ],
+            cookingTime: "420min",
+            imageURL: "http://img.sndimg.com/food/image/upload/w_266/v1/img/recipes/27/20/8/picVfzLZo.jpg",
+            originalURL: "http://www.food.com/recipe/to-die-for-crock-pot-roast-27208"
+        });
+        await insertRecipe.save()
+        console.log(`Recipe "${insertRecipe.name}" is already inserted!`);
+    } catch (error) {
+        console.log(error.message);
+        throw error
+    }
+}
 
-// insertRecipeStore()
+insertRecipeStore()
 
 
 
-// async function getRecipeStore() {
-//     try {
-//         const getRecipe = await recipesStore.findOne({ ingredients: { $elemMatch: { type: "Produce" } } })
-//         console.log(getRecipe);
+async function getRecipeStore() {
+    try {
+        const getRecipe = await recipesStore.findOne({ ingredients: { $elemMatch: { type: "Produce" } } })
+        console.log(getRecipe);
 
-//     } catch (error) {
-//         console.log(error.message);
-//         throw error
-//     }
-// }
+    } catch (error) {
+        console.log(error.message);
+        throw error
+    }
+}
 
-// getRecipeStore()
+getRecipeStore()
 
 
 
